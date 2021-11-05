cc943e10c1839d08983f6775c15cf4681b883c21
coronadito <alejandra.coronadojl@gmail.com>
2021-11-05 06:41:24 -0500

    first commit

---
+++ src/models/ruta.model.ts
@@ -0,0 +1,36 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Estacion} from './estacion.model';
+
+@model()
+export class Ruta extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  tiempo_estimado: string;
+
+  @belongsTo(() => Estacion, {name: 'origenFk'})
+  origen: string;
+
+  @belongsTo(() => Estacion, {name: 'destinoFk'})
+  destino: string;
+
+  constructor(data?: Partial<Ruta>) {
+    super(data);
+  }
+}
+
+export interface RutaRelations {
+  // describe navigational properties here
+}
+
+export type RutaWithRelations = Ruta & RutaRelations;
